d32c2ff5f57b01c9e6e6924c709c3931cfec79ad
Abhishek Joshi <44076850+abhist20@users.noreply.github.com>
2018-10-20 21:43:01 +0530

    Update move.py

--- move.py
+++ move.py
@@ -1,4 +1,4 @@
 import shutil
-src = "D:\\home\\site\\wwwroot\\uploads\\A.jpg/"
-dst = "D:\\home\\site\\wwwroot\\data/"
+src = "D:\\home\\site\\wwwroot\\uploads\\A.jpg"
+dst = "D:\\home\\site\\wwwroot\\data"
 shutil.move(src, dst)
