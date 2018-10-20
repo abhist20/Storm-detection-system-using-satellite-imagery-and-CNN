29f84aec3839676754db3ff65ce231804e6182ae
Abhishek Joshi <44076850+abhist20@users.noreply.github.com>
2018-10-20 21:51:07 +0530

    Update action.php

--- action.php
+++ action.php
@@ -1,4 +1,4 @@
- <?
-     echo exec("python D:\home\site\wwwroot\hello.py")
-     //exec("python D:\home\site\wwwroot\move.py")
+ <?php
+     //echo exec("python D:\home\site\wwwroot\hello.py")
+     exec("python D:\home\site\wwwroot\move.py")
  ?>
