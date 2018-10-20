d96468d499ac8e29cfe5bc7a37c9126c01714cfc
Abhishek Joshi <44076850+abhist20@users.noreply.github.com>
2018-10-20 19:59:28 +0530

    Create move.py

---
+++ move.py
@@ -0,0 +1,5 @@
+import os
+import shutil
+
+os.rename("path/to/current/file.foo", "path/to/new/destination/for/file.foo")
+shutil.move("path/to/current/file.foo", "path/to/new/destination/for/file.foo")
