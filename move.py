36bec26546ce188195dc0f3606fdc8ad85e77de8
Abhishek Joshi <44076850+abhist20@users.noreply.github.com>
2018-10-20 20:09:52 +0530

    Update move.py

--- move.py
+++ move.py
@@ -1,5 +1,5 @@
-import os
+#import os
 import shutil
 
-os.rename("path/to/current/file.foo", "path/to/new/destination/for/file.foo")
-shutil.move("path/to/current/file.foo", "path/to/new/destination/for/file.foo")
+#os.rename("D:\home\site\wwwroot\uploads\A.jpg", "path/to/new/destination/for/file.foo")
+shutil.move("D:\home\site\wwwroot\uploads\A.jpg", "D:\home\site\wwwroot\data\A.jpg")
