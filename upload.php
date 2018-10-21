f7092ed9db919107c5c4a06583f0b17bbb524bc2
Abhishek Joshi <44076850+abhist20@users.noreply.github.com>
2018-10-21 19:10:45 +0530

    Update upload.php

--- upload.php
+++ upload.php
@@ -32,7 +32,7 @@ if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg
 }
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.\n";
+    echo nl2br("Sorry, your file was not uploaded.\n");
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
