71090945f2a559c455119f87b5f6838f676cc747
Abhishek Joshi <44076850+abhist20@users.noreply.github.com>
2018-10-21 15:25:09 +0530

    Update action.php

--- action.php
+++ action.php
@@ -1,4 +1,4 @@
  <?php
      //echo exec("python D:\home\site\wwwroot\hello.py")
-     exec("D:\home\python364x64\python label_image.py")
+     //exec("D:\home\python364x64\python label_image.py")
  ?>
