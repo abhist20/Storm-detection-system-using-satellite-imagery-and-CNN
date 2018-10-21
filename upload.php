ac84095f770414e393cfbff5302953c3b890bbb6
Abhishek Joshi <44076850+abhist20@users.noreply.github.com>
2018-10-21 19:08:25 +0530

    Update upload.php

--- upload.php
+++ upload.php
@@ -42,5 +42,5 @@ if ($uploadOk == 0) {
         echo "Sorry, there was an error uploading your file.\n";
     }
 }
-echo "Please go back to last Page and press predict\n"
+echo nl2br("Please go back to last Page and press predict\n");
 ?>
